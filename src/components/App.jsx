96f7353767b8263722bc3a0d2fe7293882fc13c5
Olena Pohybil <itlentus@gmail.com>
2022-07-03 00:15:55 +0300

    second

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,8 @@
 import Profile from '../components/Profile/Profile';
+import Statistics from '../components/Statistics/Statistics';
+
 import user from '../data/user.json';
+import data from '../data/data.json';
 
 export const App = () => {
   return (
@@ -7,6 +10,7 @@ export const App = () => {
       style={{
         height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
@@ -20,6 +24,7 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" stats={data} />
     </div>
   );
 };
