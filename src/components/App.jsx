22c35cf00bcf50ea003994a46f49bce1bd209fed
Olena Pohybil <itlentus@gmail.com>
2022-07-03 02:55:09 +0300

    fourth

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
-import Profile from '../components/Profile/Profile';
-import Statistics from '../components/Statistics/Statistics';
+import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
 import FriendsList from './Friends/FriendsList';
+import TransactionHistory from './Transactions/TransactionHistory';
 
 import user from '../data/user.json';
 import data from '../data/data.json';
 import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
 
 export const App = () => {
     return (
@@ -27,6 +29,7 @@ export const App = () => {
             />
             <Statistics title="Upload stats" stats={data} />
             <FriendsList friends={friends} />
+            <TransactionHistory items={transactions} />
         </div>
     );
 };
